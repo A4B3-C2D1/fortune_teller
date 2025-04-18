ecbb7fa2905726272e33e15e05a48de4978b0de6
A4B3-C2D1 <21JE0563@iitism.ac.in>
2025-04-18 07:25:06 +0000

    Fortune Teller : v1.2

--- fortune.py
+++ fortune.py
@@ -6,8 +6,10 @@ print(f"\nWelcome to {NAME}'s Fortune Teller ({ADMNO})")
 # mood = input("How are you feeling today? (happy/sad/neutral): ").strip().lower()
 
 #v1.1
-mood = input("How are you feeling today? (happy/sad/neutral/stressed): ").strip().lower()
+# mood = input("How are you feeling today? (happy/sad/neutral/stressed): ").strip().lower()
 
+#v1.2
+mood = input("How are you feeling today? (happy/sad/neutral/stressed/angry): ").strip().lower()
 fortunes = {
     "happy": [
         f"Great things await you, {NAME} says! Keep smiling.",
@@ -26,13 +28,19 @@ fortunes = {
     "stressed": [
         "Take a deep breath, clarity comes when you pause.",
         "A short walk outside will reset your mind."
+    ],
+
+    #v1.2
+    "angry": [
+        "Take a moment, breathe deeply and let the tension fade.",
+        "Channel your fire into something positive, your creativity knows no bounds."
     ]
 }
 
 if mood in fortunes:
     # choose = fortunes[mood][0]
 
-    # v1.1
+    # v1.1 and v1.2
     choose = random.choice(fortunes[mood])
     
     print(f"\nYour fortune: {choose}\n")
@@ -40,7 +48,10 @@ else:
     # print("\nSorry, I don't know that mood. Please try happy, sad or neutral.\n")
 
     #v1.1
-    print("\nSorry, I don't know that mood. Please try happy, sad, neutral, or stressed.\n")
+    # print("\nSorry, I don't know that mood. Please try happy, sad, neutral, or stressed.\n")
+
+    #v1.2
+    print("\nSorry, I don't know that mood. Please try happy, sad, neutral, stressed or angry.\n")
 
 
 
